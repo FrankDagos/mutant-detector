307277d26de5b8ad32932766d8e1493c7a131dc8
Franco <francodagostino@hotmail.com>
2025-11-23 17:33:59 -0300

    Use relative path strategy for Swagger to fix 500 error

--- src/main/java/org/example/mutant_detector/config/SwaggerConfig.java
+++ src/main/java/org/example/mutant_detector/config/SwaggerConfig.java
@@ -3,7 +3,7 @@ package org.example.mutant_detector.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server; // <--- Importante
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,15 +15,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-
-                .servers(List.of(
-                        new Server().url("https://mutant-detector-api-1q1n.onrender.com").description("Servidor de Producción (Render)"),
-                        new Server().url("http://localhost:8080").description("Servidor Local")
-                ))
+                .servers(List.of(new Server().url("/")))
                 .info(new Info()
                         .title("Mutant Detector API")
                         .version("1.0")
-                        .description("API para detectar mutantes basada en análisis de secuencias de ADN. Permite identificar si una secuencia de ADN pertenece a un mutante o a un humano, y proporciona estadísticas sobre las detecciones realizadas.")
+                        .description("API para detectar mutantes basada en análisis de secuencias de ADN.")
                         .contact(new Contact()
                                 .name("Franco")
                                 .email("francodagostino20@gmail.com")));
