60baca5fdfe049cac953008c6b12bb38a801861c
Franco <francodagostino@hotmail.com>
2025-11-23 17:03:31 -0300

    Fix Swagger HTTPS issue on Render

--- src/main/java/org/example/mutant_detector/config/SwaggerConfig.java
+++ src/main/java/org/example/mutant_detector/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package org.example.mutant_detector.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server; // <--- Importante
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,6 +15,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/").description("Default Server URL")))
                 .info(new Info()
                         .title("Mutant Detector API")
                         .version("1.0")
